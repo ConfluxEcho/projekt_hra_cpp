9ff73034e0454e3565035fa22c4147b61e3e2352
ConfluxEcho <confluxecho@gmail.com>
2025-04-13 14:18:31 +0200

    Vylepšený výběr tříd, stále nedokončený, inicializace tříd

--- main.cpp
+++ main.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
 using namespace std;
 
-// Vytvoření objektu hráče (doufám že se to může)
+// Stats hráče
 struct Player {
-    int health = 100;
-    int attack = 5;
-    int speed = 1;
+    int max_health;
+    int health;
+    int max_mana;
+    int mana;
+    int attack;
 
     int money = 0;
     int inventory[10];
 };
 
-// Inicializace tříd (classes); Hodnoty jsou zatím jen placeholdery
+// Inicializace tříd (classes)
 int Classes[4][3] = {
-    {150, 5, 2}, // Class 1
-    {100, 10, 1}, // Class 2
-    {90, 5, 5}, // Class 3
-    {200, 1, 1} // Class 4
+    {120, 20, 0}, // Válečník
+    {70, 10, 100}, // Mág
+    {85, 15, 30}, // Zloděj
+    {100, 18, 60} // Paladin
 };
 
 int main(){
     Player player;
-    int choice;
+    string choice;
+    char enter;
 
     cout << """ Vítejte v teto skvělé RPG hře, kde budete objevovat vesnice, \
-bojovat proti monstrům, a bránit se proti bossům. Jako první si vyberte třídu: """;
+bojovat proti monstrům, a bránit se proti bossům. Jako první si vyberte třídu. \
+Pro pokračování stiskněte klávesu Enter.""";
+    cin >> enter;
     cout << "--------- \n";
-    cout << "Vyberte číslo 1-4: ";
-    cin >> choice;
+    while(true){
+        cout << "Třídy na výběr: Válečník; Mág; Zloděj; Paladin \n";
+        cout << "Napište jméno třídy, kterou si chcete vybrat či se o ní dozvědět více.";
+        cin >> choice;
+        if(choice == "Válečník"){
 
-    player.health = Classes[choice-1][0];
-    player.attack = Classes[choice-1][1];
-    player.speed = Classes[choice-1][2];
+        } else if(choice == "Mág"){
+
+        } else if(choice == "Zloděj"){
+
+        } else if(choice == "Paladin"){
+
+        } else {cout << "Vámi zadaná třída neexistuje, zkuste to znovu. \n"; continue;}
+
+        cout << "Chcete si opravdu vybrat tuto třídu? (Ano/Ne): ";
+        cin >>  choice;
+
+        if (tolower(choice) == "Ano"){break;} // Nefunguje
+    }
 
     return 0;
 }
