7aa920e6b40a64387b0aa0efc89513ce1efa3151
ConfluxEcho <confluxecho@gmail.com>
2025-04-18 13:29:48 +0200

    Popis a výběr tříd, přiřazení hodnot k objektu hráče podle třídy, lepší definice tříd, funkce na konvertování řetězce na malá písmena

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 // Stats hráče
@@ -9,17 +10,27 @@ struct Player {
     int mana;
     int attack;
 
-    int money = 0;
+    int money;
     int inventory[10];
 };
 
-// Inicializace tříd (classes)
-int Classes[4][3] = {
-    {120, 20, 0}, // Válečník
-    {70, 10, 100}, // Mág
-    {85, 15, 30}, // Zloděj
-    {100, 18, 60} // Paladin
+// Inicializace tříd (classes) {Životy, Škody, Mana}
+unordered_map<string, array<int, 3> > Classes = {
+    {"Válečník", {120, 20, 0}},
+    {"Mág", {70, 10, 100}},
+    {"Zloděj", {85, 15, 30}},
+    {"Paladin", {100, 18, 60}}
 };
+
+// Funkce na přeměnu řetězce na malá písmena
+string lower(string &input){
+    for(char &c : input){
+        // static_cast<unsigned char> použito pro správné chování funkce tolower(), \
+           která očekává nezápornou hodnotu typu char v ASCII tabulce
+        c = tolower(static_cast<unsigned char>(c));
+    }
+    return input;
+}
 
 int main(){
     Player player;
@@ -28,28 +39,39 @@ int main(){
 
     cout << """ Vítejte v teto skvělé RPG hře, kde budete objevovat vesnice, \
 bojovat proti monstrům, a bránit se proti bossům. Jako první si vyberte třídu. \
-Pro pokračování stiskněte klávesu Enter.""";
+Pro pokračování stiskněte klávesu Enter. """;
     cin >> enter;
-    cout << "--------- \n";
     while(true){
+        cout << "--------- \n";
         cout << "Třídy na výběr: Válečník; Mág; Zloděj; Paladin \n";
-        cout << "Napište jméno třídy, kterou si chcete vybrat či se o ní dozvědět více.";
+        cout << "Napište jméno třídy, kterou si chcete vybrat či se o ní dozvědět více. ";
         cin >> choice;
-        if(choice == "Válečník"){
+        lower(choice);
+        if(choice == "válečník"){
+            cout << "Tato třída se specializuje na útok zblízka. Má nejvíce životů \
+a dělá nejvíce škody zblízka, avšak nemá žádnou manu. \n Životy: 120; Škody: 20; Mana: 0 \n";
+        } else if(choice == "mág"){
+            cout << "Tato třída vyniká v kouzlech, jelikož má vysoký počet many a speciální schopnosti. \
+Nicméně na útok zblízka se moc nehodí. \n Životy: 70; Škody: 10; Mana: 100 \n";
+        } else if(choice == "zloděj"){
+            cout << "Pokud chcete decentní útok zblízka, ale také kouzlení, tato třída je pro vás. \
+Její speciální schopnosti vám také pomohou. \n Životy: 85; Škody: 15; Mana: 30 \n";
+        } else if(choice == "paladin"){
+            cout << "Tato třída vám bude vyhovovat pokud chcete spolehlivé healovací schopnosti. \
+Má decentní útok i manu. Životy: 100; Škody: 18; Mana: 60 \n";
+        } else {cout << "Vámi zadaná třída neexistuje, zkuste to znovu. \n --- \n"; continue;}
 
-        } else if(choice == "Mág"){
-
-        } else if(choice == "Zloděj"){
-
-        } else if(choice == "Paladin"){
-
-        } else {cout << "Vámi zadaná třída neexistuje, zkuste to znovu. \n"; continue;}
-
+        player.max_health = Classes[choice][0];
+        player.attack = Classes[choice][1];
+        player.max_mana = Classes[choice][2];
+        cout << "--- \n";
         cout << "Chcete si opravdu vybrat tuto třídu? (Ano/Ne): ";
         cin >>  choice;
 
-        if (tolower(choice) == "Ano"){break;} // Nefunguje
+        if (lower(choice) == "ano"){break;}
     }
+
+    
 
     return 0;
 }
